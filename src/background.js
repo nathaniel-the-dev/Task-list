dfa7d6c43043b646da2e72d546c1e76ef313864e
Natscamp 101 <Nathan1434>
2021-07-21 20:41:58 -0500

    Added tray functionality

--- src/background.js
+++ src/background.js
@@ -9,9 +9,65 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: true, standard: true } }]);
 
+let mainWindow, tray;
+let firstLoad = true;
+
+function createTray() {
+	try {
+		// Create tray instance
+		tray = new Tray(path.join(__static, 'favicon.png'));
+
+		const menu = Menu.buildFromTemplate([
+			{
+				label: 'Open',
+				toolTip: 'Open Task List',
+				click() {
+					mainWindow.show();
+					tray.destroy();
+				},
+			},
+			{
+				label: 'Exit',
+				toolTip: 'Exit Money Manager',
+				click() {
+					app.exit();
+				},
+			},
+		]);
+
+		// Tray variables
+		tray.setToolTip('Task List');
+		tray.setContextMenu(menu);
+
+		// Tray actions
+		tray.on('double-click', () => {
+			mainWindow.show();
+			tray.destroy();
+		});
+
+		if (firstLoad) {
+			tray.displayBalloon({ iconType: 'info', title: 'Money Manager', content: 'App minimized to tray' });
+			firstLoad = false;
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function minimizeToTray(e) {
+	// Prevent default action
+	e.preventDefault();
+
+	// Create tray icon
+	createTray();
+
+	// Hide window
+	mainWindow.hide();
+}
+
 async function createWindow() {
 	// Create the browser window.
-	const win = new BrowserWindow({
+	mainWindow = new BrowserWindow({
 		backgroundColor: '#00000017',
 		minWidth: 1280,
 		minHeight: 720,
@@ -30,17 +86,21 @@ async function createWindow() {
 		},
 	});
 
-	win.on('ready-to-show', win.show);
-
 	if (process.env.WEBPACK_DEV_SERVER_URL) {
 		// Load the url of the dev server if in development mode
-		await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-		if (!process.env.IS_TEST) win.webContents.openDevTools();
+		await mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+		if (!process.env.IS_TEST) mainWindow.webContents.openDevTools();
 	} else {
 		createProtocol('app');
 		// Load the index.html when not in development
-		win.loadURL('app://./index.html');
+		mainWindow.loadURL('app://./index.html');
 	}
+
+	mainWindow.on('ready-to-show', mainWindow.show);
+	mainWindow.on('close', minimizeToTray);
+	mainWindow.maximize();
+
+	if (!isDevelopment) Menu.setApplicationMenu(null);
 }
 
 // Quit when all windows are closed.
